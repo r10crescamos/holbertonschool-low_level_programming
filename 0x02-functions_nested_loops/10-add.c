e3922f91801ef6a432fb1453a9735aed90113926
r10crescamos <r10crescamosjuntos@gmail.com>
2021-10-05 01:37:08 -0500

    10uno1

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - two integers
+ * @n: First input in the add
+ * @g: Second input in the add
+ * Return: product
+ */
+int add(int n, int g)
+{
+	return (n + g);
+}
