18936172ac8c4308ec232a8845c406bd27c64869
r10crescamos <r10crescamosjuntos@gmail.com>
2021-10-07 18:23:53 -0500

    actdebug

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Print if numbers are positive or negative
+ * Return: Always 0
+ */
+
+void positive_or_negative(int i)
+{
+
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	else
+	{
+		printf("%d is negative\n", i);
+	}
+}
