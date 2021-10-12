e9004c486897db9d6429fd29f3914b057ae812fd
r10crescamos <r10crescamosjuntos@gmail.com>
2021-10-12 17:48:25 -0500

    07act01

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: pointer
+ */
+void puts_half(char *str)
+{
+	int n;
+	int large = 0;
+	char *ptr = str;
+	int count = 0;
+
+	while (*str != '\0')
+	{
+		large = large + 1;
+		str++;
+	}
+	if (large % 2 != 0)
+		n = (large - 1) / 2;
+	else
+		n = large / 2;
+
+	while (*ptr != '\0')
+	{
+		if (count >= n)
+			_putchar (*ptr);
+		count++;
+		ptr++;
+	}
+	_putchar('\n');
+}
