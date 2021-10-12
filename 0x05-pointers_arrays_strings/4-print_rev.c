8361e1927fcfdd243d0c228f8103ee6be59c03f3
r10crescamos <r10crescamosjuntos@gmail.com>
2021-10-12 11:04:02 -0500

    rev05act1

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - print string in reverse
+ * @s: pointer to string
+ */
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		count = count + 1;
+		s++;
+	}
+	while (count > 0)
+	{
+		_putchar(*s);
+		s--;
+		count = count - 1;
+	}
+	_putchar('\n');
+
+}
